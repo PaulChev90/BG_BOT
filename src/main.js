e3157946183ce3d47bbc130687b22f923033622e
PaulChev90 <rglokko@me.com>
2025-04-28 14:57:51 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import WarehouseList from './WarehouseList';
-import './style.css';
+import WarehouseList from './WarehouseList'; // Обрати внимание, что импортируется компонент WarehouseList
+import './style.css'; // Если ты используешь стили
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
