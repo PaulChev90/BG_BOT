4ea5fb8ca47ae7e9abd2ade33b337095ab9b925f
PaulChev90 <rglokko@me.com>
2025-04-28 14:53:44 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import WarehouseList from './WarehouseList';
 import './style.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
   <React.StrictMode>
     <WarehouseList />
-  </React.StrictMode>,
-  document.getElementById('app')
+  </React.StrictMode>
 );
