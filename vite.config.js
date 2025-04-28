e19d4f636f2ffba58320f044a6323b68c084c04e
PaulChev90 <rglokko@me.com>
2025-04-28 14:53:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Убедись, что компиляция идет на последнюю версию браузеров
+  },
 });
